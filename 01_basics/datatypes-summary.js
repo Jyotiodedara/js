1397837ffcfb3b9de6c70285ca2418eeca66ee56
Jyotiodedara <123791670+Jyotiodedara@users.noreply.github.com>
2026-08-03 13:05:35 +0000

    memoryin datatype

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -38,6 +38,28 @@ console.log(typeof myFunction);
 console.log(typeof heros);
 console.log(typeof anotherId);
 
+// Memories types:
+
+//1.stack(Primitive) when you declare variable you get a copy, 2.Heap (Non-Primitive) when any memory define under heap or any object it return refrence of original value
+
+let myYoutubename = "Khusidotcom"
+
+let anothername = myYoutubename
+anothername = "jyoticom"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+
+userTwo.email = "khushi@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
