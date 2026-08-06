98d64b877801fe7c4d0757dc72059e3801c32c40
Jyotiodedara <123791670+Jyotiodedara@users.noreply.github.com>
2026-08-06 07:54:25 +0000

    De-structure and json api

---
+++ 02_basics/object_de_structure_and_json_api.js
@@ -0,0 +1,28 @@
+const course = {
+    coursename: "javascript",
+    price: "1000",
+    Instructor: "Khushi"
+}
+
+//course.courseInstructor
+
+const{Instructor} = course
+console.log(Instructor); // o/p Khushi
+
+//API 
+//when you write directly in {} it is json kemk aa object che pn enu name nthi
+//json ma keys pn "" ma hoy and values pn "" ma.
+//{
+//    "name": "khushi",
+//    "course": "js",
+//    "price": "free"
+// }
+
+//jruri nthi k api hmesha object{} maj mde kyrek array[]ma pn mde.
+
+[
+    {},
+    {},
+    {}
+] // this is for ex. diff types of data in {}
+//use json formatar 
