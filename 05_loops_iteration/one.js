017b241b7d48d2a03bac00253b2ee124effdad5a
Jyotiodedara <123791670+Jyotiodedara@users.noreply.github.com>
2026-08-11 13:18:55 +0000

    for loop

---
+++ 05_loops_iteration/one.js
@@ -0,0 +1,22 @@
+// for loop 
+// let index = 0; (variable declare thyu eni value 0)
+//index < array.length; (condition check index ni je length che e ochi hovi joye array.length thi )
+// index++ (index ni value increase kre)
+
+// ** basic syntax
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    console.log(element)
+} // o/p 1 to 10 
+
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element)
+// } // o/p 1 to 10 
